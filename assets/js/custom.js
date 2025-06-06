b0c5901debf3b120bef6089750f71310c3f6bdfc
juan-codes45 <itsdevjuan@gmail.com>
2025-06-06 22:58:35 +0300

    initial commit

---
+++ assets/js/custom.js
@@ -0,0 +1,5 @@
+
+
+/*Code to automate changing of the year*/
+
+document.querySelector('.copyright').innerHTML = `&copy; ${new Date().getFullYear()} My Theme. Design By <a href="index.html">Developer Juan</a>. All Rights Reserved.`;
